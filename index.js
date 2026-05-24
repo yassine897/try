b6ada85d6530524c77b914471e3febd8f9089bdd
yassine897 <kryassine12@gmail.com>
2026-05-24 19:21:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let count =0
+let countEl = document.getElementById("count-el")
+
+function increment(){
+count++
+countEl.innerHTML = count
+
+
+}
+
+function reset(){
+    let count=0
+    countEl.innerHTML = count
+}
+function save(){
+
+}
